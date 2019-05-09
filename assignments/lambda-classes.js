ecf410378ff2bbf7a01970ae29c9490c29de84fb
Vaeliwen <zaman@imbriaco.com>
2019-05-09 15:45:47 -0400

    Added Instructor class extending off of Person.

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -10,3 +10,18 @@ class Person {
         return `Hello my name is ${this.name}, I am from ${this.location}.`
     }
 }
+
+class Instructor extends Person {
+    constructor(instructorAttrs) {
+        super(instructorAttrs);
+        this.favLanguage = instructorAttrs.favLanguage;
+        this.specialty = instructorAttrs.specialty;
+        this.catchPhrase = instructorAttrs.specialty;
+    }
+    demo(string) {
+        return `Today, we are learning about ${string}.`
+    }
+    grade(student, subject) {
+        return `${student.name} receives a perfect score on ${subject}.`
+    }
+}
